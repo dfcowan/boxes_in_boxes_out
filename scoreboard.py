b9aa49a95c63d8067bdafac373f4ede68210b409
Drew Cowan <dcowan@labattfood.com>
2024-09-06 16:15:28 -0500

    First stab at standings

--- scoreboard.py
+++ scoreboard.py
@@ -1,7 +1,7 @@
 import requests
 import re
 from io import BytesIO, StringIO
-from espn_api.football import League
+from espn_api.football import League, Player, Team, BoxPlayer, box_score
 from flask import Flask, request, jsonify, send_file
 scoreboard = Flask(__name__)
 
@@ -12,12 +12,10 @@ scoreboard = Flask(__name__)
 def index():
     return "<h1>Welcome to our server !!</h1>"
 
-week = 17
-
-def eligibleForPosition(position, playerPosition):
+def eligibleForPosition(position: str, playerPosition: str):
     if (position == playerPosition):
         return True
-    
+
     if (position == "FLEX" and (playerPosition == "RB" or playerPosition == "WR" or playerPosition == "TE")):
         return True
 
@@ -26,7 +24,7 @@ def eligibleForPosition(position, playerPosition):
 
     return False
 
-def printPlayer(position, player, longestNameLength):
+def printPlayer(position: str, player: Player, longestNameLength: int, week: int):
     html = ""
     stats = player.stats[week]
 
@@ -36,7 +34,7 @@ def printPlayer(position, player, longestNameLength):
 
     html += "<tr>"
     html += "<td>" + position + "</td>"
-    
+
     htmlTxt = "<td>{}</td><td>{}</td>"
     html += htmlTxt.format(player.name, player.proTeam)
 
@@ -54,7 +52,7 @@ def printPlayer(position, player, longestNameLength):
 
     return html
 
-def printTeam(team, lineup):
+def printTeam(team: Team, lineup: list[int], week: int):
     html = "<div style='border: 1px solid black;'>"
     html += "<h2>" + team.team_name + "</h2>"
     print(team.team_name)
@@ -68,7 +66,7 @@ def printTeam(team, lineup):
     for player in team.roster:
         txt = "FLEX {} {} ({})"
         txt = txt.format(player.name, player.proTeam, player.playerId)
-        
+
         if len(txt) > longestNameLength:
             longestNameLength = len(txt)
 
@@ -103,7 +101,7 @@ def printTeam(team, lineup):
     for position in positions:
         for player in team.roster:
             if player.playerId in lineup and eligibleForPosition(position, player.position) and not(player.playerId in printedLineup):
-                html += printPlayer(position, player, longestNameLength)            
+                html += printPlayer(position, player, longestNameLength, week)
                 stats = player.stats[week]
 
                 totalProjected += stats["projected_points"]
@@ -147,7 +145,7 @@ def printTeam(team, lineup):
     print(txt)
     for player in team.roster:
         if not(player.playerId in lineup):
-            html += printPlayer(player.position, player, longestNameLength)
+            html += printPlayer(player.position, player, longestNameLength, week)
 
     html += "</table>"
     html += "</div>"
@@ -155,7 +153,10 @@ def printTeam(team, lineup):
 
 
 @scoreboard.route('/finalsScoreboard', methods=['GET'])
-def respond():
+def finalsScoreboard():
+    year = 2023
+    week = 17
+
     rep = "<html>"
     rep += "<meta name='viewport' content='width=device-width'>"
     rep += "<body>"
@@ -164,41 +165,41 @@ def respond():
     rep += "<table>"
     rep += "<tr>"
     rep += "<td>"
-    leagueDev = League(league_id=1020397, year=2023)
+    leagueDev = League(league_id=1020397, year=year)
     teamDrew = leagueDev.teams[3]
     lineupDrew = [3117251, 15847, 4242335, 4360310, 2577327, 4036378, 2576414,4428331,-16013]
-    rep += printTeam(teamDrew, lineupDrew)
+    rep += printTeam(teamDrew, lineupDrew, week)
     rep += "</td>"
     rep += "</tr>"
 
     rep += "<tr>"
     rep += "<td>"
-    leagueAna = League(league_id=1953587261, year=2023)
+    leagueAna = League(league_id=1953587261, year=year)
     teamAlex = leagueAna.teams[4]
     lineupAlex = [3139477, 4697815, 4379399, 4374302, 4360438, 4361050, 2576414, 4361741, -16014]
-    rep += printTeam(teamAlex, lineupAlex)
+    rep += printTeam(teamAlex, lineupAlex, week)
     rep += "</td>"
     rep += "</tr>"
 
     rep += "<tr>"
     rep += "<td>"
-    leagueDSM = League(league_id=635993, year=2023)
+    leagueDSM = League(league_id=635993, year=year)
     teamByrd = leagueDSM.teams[3]
     lineupByrd = [3916387,3068267,3054850,3116406,4241478,3123076,4259545,4432577,-16021]
-    rep += printTeam(teamByrd, lineupByrd)
+    rep += printTeam(teamByrd, lineupByrd, week)
     rep += "</td>"
     rep += "</tr>"
 
     rep += "<tr>"
     rep += "<td>"
-    leagueMis = League(league_id=517497302, year=2023)
+    leagueMis = League(league_id=517497302, year=year)
     teamJeremy = leagueMis.teams[1]
     lineupJeremy = [2577417,3054850,4430737,4262921,4569618,4361307,3042519,4432577,-16021]
-    rep += printTeam(teamJeremy, lineupJeremy)
+    rep += printTeam(teamJeremy, lineupJeremy, week)
     rep += "</td>"
     rep += "</tr>"
     rep += "</table>"
-    
+
     rep += "</body>"
     rep += "<style>"
     rep += "table {"
@@ -210,6 +211,105 @@ def respond():
     rep += "th {"
     rep += "padding: 5px;"
     rep += "text-align: left;"
+    rep += "}"
+    rep += "</style>"
+    rep += "</html>"
+
+    return rep
+
+def sortPoints(team: Team):
+    return -1 * team.points_for
+
+def sortWins(team: Team):
+    return -1 * team.points_for
+
+def sortPlayoff(team: Team):
+    return -1 * team.playoff_pct
+
+@scoreboard.route('/standings', methods=['GET'])
+def standings():
+    year = 2024
+
+    leagueDev = League(league_id=1020397, year=year)
+    leagueAna = League(league_id=1953587261, year=year)
+    leagueDSM = League(league_id=635993, year=year)
+    leagueMis = League(league_id=517497302, year=year)
+
+    allTeams = leagueDev.teams.copy()
+    allTeams = allTeams + leagueAna.teams
+    allTeams = allTeams + leagueDSM.teams
+    allTeams = allTeams + leagueMis.teams
+
+    allTeams.sort(key=sortPlayoff)
+    allTeams.sort(key=sortPoints)
+    allTeams.sort(key=sortWins)
+
+    rep = "<html>"
+    rep += "<meta name='viewport' content='width=device-width'>"
+    rep += "<body>"
+    rep += "<h1>Boxes In, Boxes Out Standings</h1>"
+
+    rep += "<table>"
+    rep += "<thead>"
+    rep += "<tr>"
+    rep += "<th></th>"
+    rep += "<th>Team</th>"
+    rep += "<th>W</th>"
+    rep += "<th>L</th>"
+    rep += "<th>T</th>"
+    rep += "<th>Pts</th>"
+    rep += "<th>PO%</th>"
+    rep += "</tr>"
+    rep += "</thead>"
+
+
+    for (idx, team) in enumerate(allTeams):
+        rep += "<tr>"
+
+        rep += "<td>"
+        rep += str(idx + 1)
+        rep += "</td>"
+
+        rep += "<td>"
+        rep += "<img src=\"" + team.logo_url + "\" style=\"vertical-align:middle; height: 20px; width: 20px; overflow: hidden; margin-right: 5px;\" />"
+        rep += "<span>" + team.team_name + "</span> "
+        rep += "<span>(" + team.owners[0]["firstName"] + " " + team.owners[0]["lastName"] + ")</span>"
+        rep += "</td>"
+
+        rep += "<td>"
+        rep += str(team.wins)
+        rep += "</td>"
+
+        rep += "<td>"
+        rep += str(team.losses)
+        rep += "</td>"
+
+        rep += "<td>"
+        rep += str(team.ties)
+        rep += "</td>"
+
+        htmlTxt = "<td>{:10.1f}</td>"
+        rep += htmlTxt.format(team.points_for)
+
+        htmlTxt = "<td>{:10.0f}</td>"
+        rep += htmlTxt.format(team.playoff_pct)
+
+        rep += "</tr>"
+
+    rep += "</table>"
+
+    rep += "</body>"
+    rep += "<style>"
+    rep += "table {"
+    rep += "width: 100%;"
+    rep += "}"
+    rep += "td {"
+    rep += "padding: 5px;"
+    rep += "}"
+    rep += "th {"
+    rep += "padding: 5px;"
+    rep += "text-align: left;"
+    rep += "font-weight: bold;"
     rep += "}"
     rep += "</style>"
     rep += "</html>"
